46a55eb7bd17500942386814140880c5604b6b8b
sruiNascimento <sruy19@gmail.com>
2022-07-18 19:44:05 -0300

    adicionado a funcionalida para ler a variavel de ambiente

--- src/App.java
+++ src/App.java
@@ -8,16 +8,21 @@ import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // fazer um conexão e pegar os 250 melhores filmes
-        // String url = "https://imdb-api.com/en/API/Top250Movies/k_w5bdv4dn";
-        // String url = "https://imdb-api.com/en/API/MostPopularMovies/k_w5bdv4dn";
-        String url = "https://imdb-api.com/en/API/Top250TVs/k_w5bdv4dn";
-        // String url = "https://imdb-api.com/en/API/MostPopularTVs/k_w5bdv4dn";
+        //k_w5bdv4dn
+        //Top 250 Filmes
+        // String url = "https://imdb-api.com/en/API/Top250Movies/"+API_SECRET;
+        //Filmes mais populares
+        // String url = "https://imdb-api.com/en/API/MostPopularMovies/"+API_SECRET;
+        //Top 250 seriados
+        // String url = "https://imdb-api.com/en/API/Top250TVs/"+API_SECRET;
+        //Seriados mais populares
+        // String url = "https://imdb-api.com/en/API/MostPopularTVs/"+API_SECRET;
+
+        //Utilizando uma variavel de ambiente para guardar a chave da
+        final String API_SECRET = System.getenv("API_SECRET_KEY");
         
         
-        
-        // String url = "https://api.mocki.io/v2/549a5d8b";
-        // String url = "https://developers.themoviedb.org/3/trending/movie/week?api_key=ded9ce3e7ef2e209ebb01d855f779ff7";
+        String url = "https://api.mocki.io/v2/549a5d8b";
         URI endereco =  URI.create(url);
         HttpClient client = HttpClient.newHttpClient(); 
         HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
@@ -30,21 +35,13 @@ public class App {
         
         //exibir e manipular os dados
         for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println("\u001b[1m Title \u001b[0m: " + filme.get("title"));
-            System.out.println("\u001b[1m Image \u001b[0m: " + filme.get("image"));
+            System.out.println("\u001b[1m \u001b[32m  Title \u001b[0m: \u001b[40m \u001b[34m" + filme.get("title") + "\u001b[0m");
+            System.out.println("\u001b[1m \u001b[32m  Image \u001b[0m: " + filme.get("image"));
             Float avaliacao = Float.parseFloat(filme.get("imDbRating"));
-            System.out.println("\u001b[1m Rating \u001b[0m: " + geraEstrelaAvaliacao(avaliacao) );
+            System.out.println("\u001b[1m \u001b[32m  Rating \u001b[0m:  \u001b[43m" + geraEstrelaAvaliacao(avaliacao) + " \u001b[0m");
             
             System.out.println();
         }
-
-
-        //Desafios!!!
-
-        // Consumir o endpoint de filmes mais populares da API do IMDB. Procure também, na documentação da API do 
-        //IMDB, o endpoint que retorna as melhores séries e o que retorna as séries mais populares.
-
-
 
     }
 
@@ -53,7 +50,6 @@ public class App {
         String estrelas = " ";
         for (int i = 0; i < avaliacaoTruncada; i++) {
             estrelas += "★";
-            // estrelas += "\\U+2B50 ";	
         }
         return estrelas;
     }
